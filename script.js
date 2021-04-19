a2dbb771e284b9f668e786b9a8c6ba68fdc9c38d
Juan Sosa <47134417+jsosa690@users.noreply.github.com>
2021-04-18 21:31:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ function onDragStart(event) {
     event
       .dataTransfer
       .setData('text/plain', event.target.id);
+    if (event.target.parentElement.className === 'dropzone-1') {
+      updateTotal(event.target.id, 'total-1');
+    } else if (event.target.parentElement.className === 'dropzone-2') {
+      updateTotal(event.target.id, 'total-2');
     }
+  }
+
+function updateTotal(value, dropzone) {
+  let startValue = Number(document.getElementById(dropzone).innerHTML);
+  document.getElementById(dropzone).innerHTML = startValue - Number(value);
+}
 
 function onDragOver(event) {
     event.preventDefault();
@@ -16,20 +26,25 @@ function onDrop(event) {
     const dropzone = event.target;
     if(!dropzone.draggable) {
         dropzone.appendChild(draggableElement);
+        updateSum(draggableElement.id, dropzone)
     } else {
         dropzone.parentElement.appendChild(draggableElement);
+        updateSum(draggableElement.id, dropzone.parentElement)
     }
     event.dataTransfer.clearData();
 }
 
+function updateSum(value, dropzone) {
+  if (dropzone.className === 'dropzone-1') {
+    let startValue = Number(document.getElementById("total-1").innerHTML);
+    document.getElementById("total-1").innerHTML = startValue + Number(value);
+  } else if (dropzone.className === 'dropzone-2') {
+    let startValue = Number(document.getElementById("total-2").innerHTML);
+    document.getElementById("total-2").innerHTML = startValue + Number(value);
+  }
+  
+}
 
-window.addEventListener(
-    "scroll",
-    () => {
-      document.body.style.setProperty(
-        "--scroll",
-        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-      );
-    },
-    false
-  );
+window.addEventListener('scroll', () => {
+  document.body.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+}, false);
